aecb6397339c0e9734bebf5ae93928a29fda5947
oboril <jan.oboril@gmail.com>
2023-10-11 22:45:22 +0200

    Added custom priority queue for compatibility

--- numerical_integration.cs
+++ numerical_integration.cs
@@ -1,6 +1,3 @@
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
-
 namespace NumericalIntegration
 {
     public struct IntegrationResult
@@ -8,7 +5,7 @@ namespace NumericalIntegration
         public double integral;
         public double error;
 
-        public IntegrationResult(double _integral, double _error) 
+        public IntegrationResult(double _integral, double _error)
         {
             integral = _integral;
             error = _error;
@@ -28,6 +25,91 @@ namespace NumericalIntegration
             b = _b;
             integral = _integral;
             error = _error;
+        }
+    }
+
+    /// <summary>
+    /// Max heap that stores the integration regions such that the region with maximum error is easily accessible
+    /// </summary>
+    struct IntegrationRegionHeap
+    {
+        public List<IntegrationRegion> container;
+
+        public IntegrationRegionHeap(){
+            container = new List<IntegrationRegion>();
+        }
+
+        private int parent(int x)
+        {
+            return (x - 1) / 2;
+        }
+        private int child_left(int x)
+        {
+            return x * 2 + 1;
+        }
+        private int child_right(int x)
+        {
+            return x * 2 + 2;
+        }
+        private void swap(int x, int y)
+        {
+            (container[x], container[y]) = (container[y], container[x]);
+        }
+
+        public void add(IntegrationRegion item)
+        {
+            container.Add(item);
+            int idx = container.Count() - 1;
+            while (idx > 0)
+            {
+                int p = parent(idx);
+                if (container[idx].error <= container[p].error) break;
+                swap(idx, p);
+                idx = p;
+            }
+        }
+
+        public IntegrationRegion peek()
+        {
+            return container[0];
+        }
+
+        public IntegrationRegion pop()
+        {
+            // get first item
+            IntegrationRegion reg = container[0];
+
+            // replace it with last item
+            int n = container.Count() - 1;
+            container[0] = container[n];
+            container.RemoveAt(n);
+
+            // make sure the heap is valid
+            int idx = 0;
+            int child_l = child_left(idx);
+            int child_r = child_right(idx);
+            while (child_l < n)
+            {
+                if (child_r < n && container[child_r].error > container[child_l].error && container[child_r].error > container[idx].error)
+                {
+                    swap(idx, child_r);
+                    idx = child_r;
+
+                }
+                else if (container[child_l].error > container[idx].error)
+                {
+                    swap(idx, child_l);
+                    idx = child_l;
+                }
+                else {
+                    break;
+                }
+                child_l = child_left(idx);
+                child_r = child_right(idx);
+            }
+
+            // return the popped element
+            return reg;
         }
     }
 
@@ -87,7 +169,7 @@ namespace NumericalIntegration
 
         private static IntegrationRegion fixed_quadrature(Func<double, double> func, double a, double b)
         {
-            double ba2 = (b-a)/2;
+            double ba2 = (b - a) / 2;
 
             double gauss = 0;
             double kronrod = 0;
@@ -102,7 +184,7 @@ namespace NumericalIntegration
             gauss *= ba2;
             kronrod *= ba2;
 
-            double error = Math.Abs(kronrod-gauss);
+            double error = Math.Abs(kronrod - gauss);
 
             return new IntegrationRegion(a, b, kronrod, error);
         }
@@ -129,31 +211,31 @@ namespace NumericalIntegration
             // The quadrature is 15-point, thus adjust minimal and maximal step
             max_step *= 15;
 
-            PriorityQueue<IntegrationRegion, double> regions = new PriorityQueue<IntegrationRegion, double>();
+            IntegrationRegionHeap regions = new IntegrationRegionHeap();
 
             double integral = 0;
             double error = 0;
 
             // Create initial integration regions according to maximum step size
-            double ba = b-a;
-            int init_regions = (int)Math.Ceiling(ba/max_step);
+            double ba = b - a;
+            int init_regions = (int)Math.Ceiling(ba / max_step);
             if (init_regions < 1) init_regions = 1;
-            double init_region_size = ba/init_regions;
+            double init_region_size = ba / init_regions;
             double ai = a;
             double bi = ai + init_region_size;
             IntegrationRegion region;
             for (int i = 0; i < init_regions - 1; i++)
             {
                 region = fixed_quadrature(func, ai, bi);
-                regions.Enqueue(region, -region.error);
+                regions.add(region);
 
                 integral += region.integral;
                 error += region.error;
 
-                (ai, bi) = (bi, bi+init_region_size);
+                (ai, bi) = (bi, bi + init_region_size);
             }
             region = fixed_quadrature(func, ai, b);
-            regions.Enqueue(region, -region.error);
+            regions.add(region);
             integral += region.integral;
             error += region.error;
 
@@ -161,7 +243,7 @@ namespace NumericalIntegration
             // Check convergence and subdivide region with largest error
             while (error > epsabs && error / Math.Abs(integral) > epsrel)
             {
-                IntegrationRegion old_region = regions.Dequeue();
+                IntegrationRegion old_region = regions.pop();
 
                 // Check that the region size is large enough for numerical accuracy
                 if (old_region.b - old_region.a < Math.Max(Math.Abs(old_region.a), Math.Abs(old_region.b)) * 1e-14)
@@ -169,23 +251,22 @@ namespace NumericalIntegration
                     Console.WriteLine("WARNING: The integration step is smaller that spacing between numbers!");
                     break;
                 }
-                double midpoint = (old_region.a + old_region.b)/2;
+                double midpoint = (old_region.a + old_region.b) / 2;
                 IntegrationRegion r1 = fixed_quadrature(func, old_region.a, midpoint);
                 IntegrationRegion r2 = fixed_quadrature(func, midpoint, old_region.b);
-                regions.Enqueue(r1, -r1.error);
-                regions.Enqueue(r2, -r2.error);
+                regions.add(r1);
+                regions.add(r2);
 
                 integral += r1.integral + r2.integral - old_region.integral;
                 error += r1.error + r2.error - old_region.error;
             }
-            
-            Console.WriteLine("Integration regions: " + regions.Count.ToString());
+
+            Console.WriteLine("Integration regions: " + regions.container.Count().ToString());
 
             // Use the regions to recalculate integral and error to avoid rounding error
             integral = 0;
             error = 0;
-            IntegrationRegion final_region;
-            while (regions.TryDequeue(out final_region, out _))
+            foreach (IntegrationRegion final_region in regions.container)
             {
                 integral += final_region.integral;
                 error += final_region.error;
